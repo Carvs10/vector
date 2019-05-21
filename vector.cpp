48ba99dc074a12ecad623d3794bb454e2f44079f
Pedro Henrique Alves Cardoso <pedro.cardoso@ufrn.edu.br>
2019-05-21 01:09:18 -0300

    update

---
+++ vector.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+template << typename
+
+class sala{
+	
+	public:
+		sala( int num_pessoas=0, std::string professor="sem_nome"){
+		
+		this->num_pessoas= num_pessoas;
+		this->professor = professor;
+		std::cout<< "construtor\n";
+		}
+
+		~sala(){
+		std::cout<<"entrei no destrutor\n"
+		}
+
+		int pegarnumpessoas();
+
+	private:
+		int num_pessoas;
+		std::string  professor;
+};
+int main(){
+	sala a212;
+	sala a305(10, "ivan");
+
+	std::cout << a305.pegarnumpessoas() << std::endl;
+}
+
+int sala::pegarnumpessoas(){
+	AFAFASFAFASFA
+}
